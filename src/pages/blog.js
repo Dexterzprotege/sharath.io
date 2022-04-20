3cca1992e9a7158273300fa6368becf5e8ab7704
Sharath Kamlekar <skamleka@amazon.com>
2022-04-20 17:39:34 +0530

    Trigger build

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -12,7 +12,7 @@ export default function Blog() {
         {/* + Title */}
         <div className="row mt-4 justify-content-center">
           <div className="col-lg-7 col-sm-12 my-auto headline">
-            <h1>Hi from Blog, content is coming soon!</h1>
+            <h1>Hi from Blog, content is coming soon!!</h1>
           </div>
         </div>
         <div className="col-lg-6 my-auto mx-auto">
